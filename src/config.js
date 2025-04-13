b56c3931272c1bb5aa7c5e90ae4a1e8abd0be096
obscuredusername <zubayrr1011@gmail.com>
2025-04-13 23:08:37 +0500

    minor fix

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "https://bazaarbackend-production-0002.up.railway.app:8080"
+const API_BASE_URL = "https://bazaarbackend-production-0002.up.railway.app"
 
 export default API_BASE_URL
